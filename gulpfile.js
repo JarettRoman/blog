a460f9f55c416bbebef4d3bf9d5b412da5123204
JarettRoman <afrocious@gmail.com>
2021-05-04 00:22:23 -0400

    set up sass compiler, watcher, and browser-sync reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-dart-sass');
+const browserSync = require('browser-sync').create();
 
 function css() {
-    return src('public/src/scss/styles.scss')
+    return src('./public/src/scss/styles.scss')
     .pipe(sass())
-    .pipe(dest('public/dist/css'))
+    .pipe(dest('./public/dist/css'))
+}
+
+function serve(done) {
+    browserSync.init({
+        server: {
+            baseDir: './public/dist'
+        }
+    })
+    done();
+}
+
+function reload(done) {
+    browserSync.reload();
+    done();
 }
 
 function watcher() {
-    watch('public/src/scss/styles.scss', series(css));
+    watch('./public/src/**/*.*', series(css, reload));
 }
 
 exports.css = css;
+exports.serve = serve;
 exports.watcher = watcher;
+exports.init = series(css, serve, watcher)
--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const path = require('path');
 const PORT = 4000;
 const app = new express();
 
-app.use(express.static('public'));
+app.use(express.static('/public/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-});
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+    res.sendFile(path.resolve(__dirname, 'public/dist/index.html'))
 });
